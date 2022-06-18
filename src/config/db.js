121a288f2de157363be9800420e1695673162332
Oscar <orodriguezc1603@alumno.ipn.mx>
2022-06-17 22:09:08 -0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,8 @@
+import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import { logger } from '../logs/loggers.logs.js';
+
+dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI, (err) => {
 	if (err) {
